7845a0924e6f637bce7f8ee1c0e8ceaa2e983422
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 11:47:25 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
+++ src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barclays.cloud.demo.model.Employee;
@@ -31,6 +33,12 @@ public class EmployeeController {
 	@GetMapping("/get-all-emps")
 	public List<Employee> getAllEmps() {
 		return empService.getAllEmployees();
+	}
+
+	@PostMapping("/add-emp")
+	public Employee addEmp(@RequestBody Employee employee) {
+		System.out.println(employee.toString());
+		return employee;
 	}
 
 }
