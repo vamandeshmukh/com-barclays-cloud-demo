1804560be50b77b3b5b849a89837ac8b9510470f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 09:50:00 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/HelloController.java
+++ src/main/java/com/barclays/cloud/demo/controller/HelloController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+//	http://localhost:9999/hello
+	
 	@GetMapping("/hello")
 	public String hello() {
 		System.out.println("hello");
 		return "Hello world!";
 	}
 
+//	http://localhost:9999/hi
+	
+	@GetMapping("/hi")
+	public String hi() {
+		System.out.println("hi");
+		return "Hi buddies!";
+	}
 }
