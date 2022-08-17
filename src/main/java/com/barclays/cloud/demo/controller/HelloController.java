0881c982aa6ae368590a4d8fd7f7518eedcc8e4b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 09:25:08 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/HelloController.java
+++ src/main/java/com/barclays/cloud/demo/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.barclays.cloud.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+	@GetMapping("/hello")
 	public String hello() {
 		System.out.println("hello");
 		return "Hello world!";
