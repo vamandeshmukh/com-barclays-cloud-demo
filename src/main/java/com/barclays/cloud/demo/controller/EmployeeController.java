ca55cbf4c3958a50799a0b233f023b3597388b97
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 11:23:00 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
+++ src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barclays.cloud.demo.model.Employee;
@@ -17,10 +18,12 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService empService;
 
-	// http://localhost:9999/get-emp-by-id
+	// http://localhost:9999/get-emp-by-id/{eid}
+	// http://localhost:9999/get-emp-by-id/101
 
-	@GetMapping("/get-emp-by-id")
-	public Employee getEmpById() {
+	@GetMapping("/get-emp-by-id/{eid}")
+	public Employee getEmpById(@PathVariable(name = "eid") int employeeId) {
+		System.out.println(employeeId);
 		return empService.getEmployeeById();
 	}
 
