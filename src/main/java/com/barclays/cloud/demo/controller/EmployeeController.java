5e3047acce24da3158d904a290e22032cbd73b3c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 12:03:21 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
+++ src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
@@ -3,9 +3,9 @@ package com.barclays.cloud.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,9 +25,9 @@ public class EmployeeController {
 	// http://localhost:9999/get-emp-by-id/{eid}
 
 //	@GetMapping("/get-emp-by-id/{eid}")
-//	@RequestMapping(name = "/get-emp-by-id/{eid}", method = RequestMethod.GET)
-	@RequestMapping(name = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = { "application/json" })
-
+//	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET)
+//	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = { "application/json" })
 	public Employee getEmpById(@PathVariable(name = "eid") int employeeId) {
 		System.out.println(employeeId);
 		return empService.getEmployeeById();
@@ -43,8 +43,7 @@ public class EmployeeController {
 //	http://localhost:9999/add-emp 
 
 //	@PostMapping("/add-emp")
-	@RequestMapping(name = "/get-emp-by-id/{eid}", method = RequestMethod.POST, produces = {
-			"application/json" }, consumes = { "application/json" })
+	@RequestMapping(value = "/add-emp", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Employee addEmp(@RequestBody Employee employee) {
 		System.out.println(employee.toString());
 		return employee;
