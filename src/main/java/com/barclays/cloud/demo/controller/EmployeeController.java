0af18f35b6f743515735d6b1c8de16adbeb875c5
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 10:38:30 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
+++ src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
@@ -7,31 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barclays.cloud.demo.model.Employee;
+import com.barclays.cloud.demo.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
+
+	EmployeeService empService;
 
 //	http://localhost:9999/get-emp-by-id
 
 	@GetMapping("/get-emp-by-id")
 	public Employee getEmpById() {
-		Employee emp = new Employee(101, "Sonu", 90000);
-		System.out.println(emp.toString());
-		return emp;
+		return empService.getEmployeeById();
 	}
 
 //	http://localhost:9999/get-all-emps
 
 	@GetMapping("/get-all-emps")
 	public List<Employee> getAllEmps() {
-
-		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 90000));
-		empList.add(new Employee(102, "Monu", 95000));
-		empList.add(new Employee(103, "Tonu", 85000));
-
-		empList.forEach(e -> System.out.println(e.toString()));
-		return empList;
+		return empService.getAllEmployees();
 	}
 
 }
--- src/main/java/com/barclays/cloud/demo/service/EmployeeService.java
+++ src/main/java/com/barclays/cloud/demo/service/EmployeeService.java
@@ -1,5 +1,25 @@
 package com.barclays.cloud.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.barclays.cloud.demo.model.Employee;
+
 public class EmployeeService {
+
+	public Employee getEmployeeById() {
+		Employee emp = new Employee(101, "Sonu", 90000);
+		System.out.println(emp.toString());
+		return emp;
+	}
+
+	public List<Employee> getAllEmployees() {
+		List<Employee> empList = new ArrayList<>();
+		empList.add(new Employee(101, "Sonu", 90000));
+		empList.add(new Employee(102, "Monu", 95000));
+		empList.add(new Employee(103, "Tonu", 85000));
+		empList.forEach(e -> System.out.println(e.toString()));
+		return empList;
+	}
 
 }
