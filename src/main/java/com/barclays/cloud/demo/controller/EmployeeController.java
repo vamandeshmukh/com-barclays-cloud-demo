766bc9534f23e95a7f08ee8edb0d9a9c63137ee1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-17 12:28:48 +0530

    spring boot

--- src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
+++ src/main/java/com/barclays/cloud/demo/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package com.barclays.cloud.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,34 +18,32 @@ import com.barclays.cloud.demo.model.Employee;
 import com.barclays.cloud.demo.service.EmployeeService;
 
 @RestController
+@RequestMapping("/emp")
 public class EmployeeController {
-
-//	EmployeeService empService = new EmployeeService();
 
 	@Autowired
 	EmployeeService empService;
 
-	// http://localhost:9999/get-emp-by-id/{eid}
+	// http://localhost:9999/emp/get-emp-by-id/{eid}
 
-//	@GetMapping("/get-emp-by-id/{eid}")
-//	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET)
-//	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = { "application/json" })
-	public Employee getEmpById(@PathVariable(name = "eid") int employeeId) {
-		System.out.println(employeeId);
-		return empService.getEmployeeById();
+	public ResponseEntity<Employee> getEmpById(@PathVariable(name = "eid") int employeeId) {
+		Employee emp = empService.getEmployeeById();
+		HttpStatus status = HttpStatus.OK;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "employee with eid " + employeeId + " returned successfully.");
+		ResponseEntity<Employee> response = new ResponseEntity<>(emp, headers, status);
+		return response;
 	}
 
-//	http://localhost:9999/get-all-emps
-
+	@RequestMapping(value = "/get-all-emps", method = RequestMethod.GET, produces = { "application/json" })
 	@GetMapping("/get-all-emps")
 	public List<Employee> getAllEmps() {
 		return empService.getAllEmployees();
 	}
 
-//	http://localhost:9999/add-emp 
+//	http://localhost:9999/emp/add-emp 
 
-//	@PostMapping("/add-emp")
 	@RequestMapping(value = "/add-emp", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Employee addEmp(@RequestBody Employee employee) {
 		System.out.println(employee.toString());
@@ -50,3 +51,58 @@ public class EmployeeController {
 	}
 
 }
+
+//package com.barclays.cloud.demo.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.barclays.cloud.demo.model.Employee;
+//import com.barclays.cloud.demo.service.EmployeeService;
+//
+//@RestController
+//@RequestMapping("/emp")
+//public class EmployeeController {
+//
+////	EmployeeService empService = new EmployeeService();
+//
+//	@Autowired
+//	EmployeeService empService;
+//
+//	// http://localhost:9999/get-emp-by-id/{eid}
+//	// http://localhost:9999/emp/get-emp-by-id/{eid}
+//
+////	@GetMapping("/get-emp-by-id/{eid}")
+////	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET)
+////	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//	@RequestMapping(value = "/get-emp-by-id/{eid}", method = RequestMethod.GET, produces = { "application/json" })
+//	public Employee getEmpById(@PathVariable(name = "eid") int employeeId) {
+//		System.out.println(employeeId);
+//		return empService.getEmployeeById();
+//	}
+//
+////	http://localhost:9999/get-all-emps
+//
+//	@GetMapping("/get-all-emps")
+//	public List<Employee> getAllEmps() {
+//		return empService.getAllEmployees();
+//	}
+//
+////	http://localhost:9999/add-emp 
+//
+////	@PostMapping("/add-emp")
+//	@RequestMapping(value = "/add-emp", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+//	public Employee addEmp(@RequestBody Employee employee) {
+//		System.out.println(employee.toString());
+//		return employee;
+//	}
+//
+//}
